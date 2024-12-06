a568ffe374e876d1ba5854b06caf5579e6a61c6b
TNCHZ <dochuong2222@gmail.com>
2024-12-07 00:20:26 +0700

    jk

---
+++ phongmachtu/phongmachtu/index.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from phongmachtu import app
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
